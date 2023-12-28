c5728a9170b6426bebc6ab99b2f841345c7fa77b
DaveyDark <devesh97531@gmail.com>
2023-12-28 15:54:08 +0530

    Layout update

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
   <div>
     <h1 className='h1-bold'>Home</h1>
-      <UserButton />
+    <UserButton />
   </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,28 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import React from 'react'
+import {Inter, Space_Grotesk} from 'next/font/google'
+import type {Metadata} from 'next'
+
+export const metadata: Metadata = { 
+  title: "DevFlow",
+  description: "A community driven platform for developers to ask questions or answer asked questions and share their knowledge.",
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+}
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+
+const space_grotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'], 
+  variable: '--font-spaceGrotesk',
+})
  
 export default function RootLayout({
   children,
@@ -8,9 +30,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider 
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient', 
+          footerActionLink: 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${space_grotesk.variable}`}>
+          <h1 className="h1-bold">This is a header</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
